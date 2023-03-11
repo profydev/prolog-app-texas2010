132250e21e9691e1867442096bfef1828ae5fb1c
texas2010 <9701609+texas2010@users.noreply.github.com>
2023-03-10 22:09:00 -0600

    Support button should have a valid mailto link

--- cypress/e2e/navigation.cy.ts
+++ cypress/e2e/navigation.cy.ts
@@ -36,7 +36,7 @@ describe("Sidebar Navigation", () => {
       cy.get("nav").contains("Collapse").click();
 
       // check that links still exist and are functionable
-      cy.get("nav").find("a").should("have.length", 5).eq(1).click();
+      cy.get("nav").find("a").should("have.length", 6).eq(1).click();
       cy.url().should("eq", "http://localhost:3000/dashboard/issues");
 
       // check that text is not rendered
@@ -80,7 +80,7 @@ describe("Sidebar Navigation", () => {
       isInViewport("nav");
 
       // check that all links are rendered
-      cy.get("nav").find("a").should("have.length", 5);
+      cy.get("nav").find("a").should("have.length", 6);
 
       // Support button should be rendered but Collapse button not
       cy.get("nav").contains("Support").should("exist");
@@ -91,5 +91,15 @@ describe("Sidebar Navigation", () => {
       cy.wait(500);
       isNotInViewport("nav");
     });
+  });
+
+  it("Support button should have a valid mailto link", () => {
+    cy.get("nav")
+      .contains("Support")
+      .should(
+        "have.attr",
+        "href",
+        "mailto:support@prolog-app.com?subject=Support%20Request"
+      );
   });
 });
